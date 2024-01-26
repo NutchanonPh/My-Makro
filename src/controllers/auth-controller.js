7bd1f491e3d3c9dcadc02ec9fa98afef3a3673cd
NutchanonPh <nutchanon.ph@cs.rmu.ac.th>
2024-01-26 10:09:36 +0700

    My-Makro

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,14 +1,69 @@
-exports.register = (req, res, next) => {
+const createError = require("../utils/createError");
+const prisma = require("../config/prisma");
+const bcrypt = require("bcryptjs");
+const userService = require("../services/user-service");
+const jwt = require("jsonwebtoken");
+
+exports.register = async (req, res, next) => {
+try{
   const { email, password } = req.body;
 
-  //   logic
+  if (!email || !password) {
+    return createError(400, "Email and password are required");
+  }
+  
+  if (typeof email !== "string" || typeof password !== "string") {
+    return createError(400, "Email or password is invalid");
+  }
 
-  res.json({ email, password });
+  const isUserExist = await userService.getUserByEmail(email);
+
+  if(isUserExist) {
+    return createError(400, "User already exist");
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  await userService.createUser(email, hashedPassword);
+
+  res.json({message: "Register Success" });
+  } catch(err) {
+    next(err);
+  }
 };
 
-exports.login = (req, res, next) => {
+exports.login = async (req, res, next) => {
+try {
   const { email, password } = req.body;
-  res.json({ email, password });
+
+  if(!email || !password) {
+    return createError(400, "Email and password required");
+  }
+
+  if(typeof email !== "string" || typeof password !== "string") {
+    return createError(400, "Email or password is invaild");
+  }
+
+  const isUserExist = await userService.getUserByEmail(email);
+
+  if(!isUserExist) {
+    return createError(400, "Email or passsword is invalid");
+  }
+
+  const isPasswordMatch = bcrypt.compare(passsword, isUserExist.passsword);
+
+  if(!isPasswordMatch) {
+    return createError(400, "Email or password is invalid");
+  }
+
+  const token = jwt.sign({id: isUserExist.id}, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
+    res.json({ token });
+  } catch(err) {
+    next(err);
+  }
 };
 
 exports.forgetPassword = (req, res, next) => {
